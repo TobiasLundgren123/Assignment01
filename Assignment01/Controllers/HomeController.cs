0379a9bf4de5f9ecafe39c9ce2f61bf052dbd5a6
TobiasLundgren123 <tobias_lundgren@yahoo.com>
2022-11-07 18:40:33 +0100

    Add project files.

---
+++ Assignment01/Controllers/HomeController.cs
@@ -0,0 +1,38 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Assignment01.Controllers
+{
+    public class HomeController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult About()
+        {
+            return View();
+        }
+
+        public IActionResult Contact()
+        {
+            return View();
+        }
+
+        public IActionResult Projects()
+        {
+            return View();
+        }
+
+        //private DateTime _returnDate = DateTime.MinValue;
+        //public DateTime ReturnDate
+        //{
+        //    get
+        //    {
+        //        return (_returnDate == DateTime.MinValue) ? DateTime.Now() : _returnDate;
+        //    }
+        //    set { _returnDate = value; }
+        //}
+
+    }
+}
